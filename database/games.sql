a65387fef22f95158e0a85ada2b73374964cfd92
Ana Júlia <anajuliaalmeidamachado2@gmail.com>
2026-03-24 22:37:01 -0300

    Create games.sql

---
+++ database/games.sql
@@ -0,0 +1,91 @@
+-- TAB_JOGOS: (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario)
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Super Mario Bros.', NULL, 'Plataforma', 'LIVRE', '1985-09-13', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Tetris', NULL, 'Puzzle', 'LIVRE', '1984-06-06', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Pokémon Red / Blue', NULL, 'RPG', 'LIVRE', '1996-02-27', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Super Mario 64', NULL, 'Plataforma 3D', 'LIVRE', '1996-06-23', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Super Mario Odyssey', NULL, 'Plataforma / Aventura', 'LIVRE', '2017-10-27', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Chrono Trigger', NULL, 'RPG', 'LIVRE', '1995-03-11', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Super Smash Bros. Ultimate', NULL, 'Luta', '10 anos', '2018-12-07', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('The Legend of Zelda: Ocarina of Time', NULL, 'Aventura', '10 anos', '1998-11-21', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('The Legend of Zelda: Breath of the Wild', NULL, 'Aventura / Mundo Aberto', '10 anos', '2017-03-03', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Minecraft', NULL, 'Sandbox / Criativo', '12 anos', '2011-11-18', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('League of Legends', NULL, 'MOBA', '12 anos', '2009-10-27', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Genshin Impact', NULL, 'RPG / Aventura', '12 anos', '2020-09-28', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Overwatch', NULL, 'FPS / Online', '12 anos', '2016-05-24', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Fortnite', NULL, 'Battle Royale', '12 anos', '2017-07-21', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Metal Gear Solid 3: Snake Eater', NULL, 'Stealth / Ação', '14 anos', '2004-11-17', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Persona 5 Royal', NULL, 'RPG', '14 anos', '2019-10-31', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Dark Souls', NULL, 'RPG / Ação', '16 anos', '2011-09-22', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Elden Ring', NULL, 'RPG / Mundo Aberto', '16 anos', '2022-02-25', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Half-Life 2', NULL, 'FPS', '16 anos', '2004-11-16', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Bloodborne', NULL, 'Ação / RPG', '16 anos', '2015-03-24', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('The Elder Scrolls V: Skyrim', NULL, 'RPG / Mundo Aberto', '16 anos', '2011-11-11', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Counter-Strike: Global Offensive', NULL, 'FPS / Competitivo', '16 anos', '2012-08-21', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('The Witcher 3: Wild Hunt', NULL, 'RPG', '18 anos', '2015-05-19', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Red Dead Redemption 2', NULL, 'Ação / Mundo Aberto', '18 anos', '2018-10-26', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('The Last of Us Part I', NULL, 'Ação / Drama', '18 anos', '2013-06-14', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Grand Theft Auto V', NULL, 'Ação / Sandbox', '18 anos', '2013-09-17', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('God of War', NULL, 'Ação / Mitologia', '18 anos', '2018-04-20', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Resident Evil 4', NULL, 'Terror / Ação', '18 anos', '2005-01-11', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('DOOM', NULL, 'FPS', '18 anos', '1993-12-10', 1);
+
+INSERT INTO TAB_JOGOS (nm_jogo, img_jogo, genero, classificacao, dt_jogo, id_usuario) 
+VALUES ('Bioshock', NULL, 'FPS / Narrativo', '18 anos', '2007-08-21', 1);
